5202c84f26754f4dabb1fb2620458b18d0f92382
Kelly Alves <kellytalvesp@gmail.com>
2025-08-04 19:18:05 -0300

    Teste de arquivos
    apenas testando para divulgação externa

---
+++ script.js
@@ -0,0 +1,8 @@
+function copyEmail() {
+  navigator.clipboard.writeText('kellytalvesp@gmail.com');
+  alert('E-mail copiado para a área de transferência!');
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark');
+}
